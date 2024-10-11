a00d57fea360d0daef0ddb159214298e19e3cd71
graceabiolu838 <grace.olusegun@laetottenham.ac.uk>
2024-10-11 10:53:23 +0000

    Pending changes exported from your codespace

--- currency_exchange_tool.py
+++ currency_exchange_tool.py
@@ -1,13 +1,35 @@
-import math # you'll probably need this
+#currency will be input in main program 
+def valid_price(price):
+    global currency
+    if price<10 or price>1000:
+        return False
+    else:
+        return price
 
-exchange_rates = {
-    'USD': 1.13, #I.E. 1 Pound is 1.13 Dollars
-    'EUR': 1.15,
-}
 
-def check_currency_exists(currency):
-    return
+curr_rate = {"USD": 1.3, "EURO": 1.2, "CAD": 1.8, "NAIRA": 2121, "COP": 5543}
 
-def currency_convert(original_c, new_c, amount):
-    # your code here
-    return
+def curr_convert(currency, price):
+    global curr_rate
+    if currency in curr_rate:
+        rate = curr_rate[currency]
+        result = round(rate*price,2)
+        return str(price) + " in " + currency + " is " + str(result)
+    else:
+        return False
+#######input in main code    
+price = int(input("How much to pay\n"))     
+x = valid_price(price)
+
+if x == False:
+    print("Invalid price\n")
+else:
+    currency = input("What currency do you want: " + str(list(curr_rate.keys())) + "\n")
+    y = curr_convert(currency, price)
+    if y == False:
+        print("Invalid currency\n")
+    else:
+        print(y)
+
+    
+
