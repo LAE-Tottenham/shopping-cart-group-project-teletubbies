d35d532c2d2a7baaf287eb5e7f41a5f4923db7f6
perusemail <159719043+perusemail@users.noreply.github.com>
2024-10-11 10:41:29 +0000

    Pending changes exported from your codespace

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
 from currency_exchange_tool import currency_convert
-from shop_functions import start_shop
-
-print('Welcome to my shop')
-
-while True:
-    print('Please select what you would like to buy')
-    items_to_buy = start_shop()
-
-    # blah blah 
+from shop_functions import delivery, delivery_day_time
--- shop_functions.py
+++ shop_functions.py
@@ -1,19 +1,32 @@
-items = {
-    'Bread': 1.20,
-    'Milk': 1.15,
-    'Chocoloate': 0.5,
-}
 
-def start_shop():
-    while True:
-        print("What you would like to buy? ")
-        # display items to buy
 
-    # blah blah blah
 
-    return {
-        'items': [],
-        'total_cost': 0
-    }
-        
-        
+def delivery(postcode):
+    price = ord(postcode[0]) * 2
+    return price
+
+def delivery_day_time():
+    from datetime import date
+    today = date.today()
+    delivery_date = int(today.day) + 7 
+    print("Choose a slot for delivery on",delivery_date,"\n1) 9:00AM\n2) 12:00PM\n3) 18:00PM")
+    choice = int(input())
+    times = [
+        [1 , "9:00AM"],
+        [2 , "12:00PM"],
+        [3 , "18:00PM"]
+    ]
+    while choice not in [1,2,3]:
+        print("Choose a slot for delivery on",delivery_date,"\n1) 9:00AM\n2) 12:00PM\n3) 18:00PM")
+        choice = int(input())
+
+    for counter in times:
+        if choice == counter[0]:
+            timeslot = counter[1]
+
+    return [delivery_date,timeslot]
+    # delivery details are stored as array [day,timeslot] (use index [0] for day and [1] for time)
+
+
+
+print(delivery_day_time())
